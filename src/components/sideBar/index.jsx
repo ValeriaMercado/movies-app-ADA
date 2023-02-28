1ab45167cab3cf3bc7a7847227a66255e2e97dbd
María Macarena Alvarez Castillo <mariamacarenaac@gmail.com>
2023-02-28 17:52:25 -0300

    still working on sidebar

--- src/components/sideBar/index.jsx
+++ src/components/sideBar/index.jsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react'
+import { Avatar, Divider, Flex, Heading } from '@chakra-ui/react'
 
 export const SideBar = () => {
   return (
@@ -8,11 +8,36 @@ export const SideBar = () => {
       h="95vh"
       marginTop="2.5vh"
       boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
-      borderRadius={navSize == "small" ? "15px" : "30px"}
+      borderRadius= "30px"
       w="200px"
       flexDir="column"
       justifyContent="space-between"
     >
+      <Flex>
+        <IconButton
+          background="none"
+          mt={5}
+          _hover={{ background: 'none' }}
+          icon={<FiMenu />}
+          onClick={() => {}}
+        />
+      </Flex>
+      <Flex
+        p="5%"
+        flexDir="column"
+        w="100%"
+        alignItems="flex-start"
+        mb={4}
+      >
+        <Divider/>
+        <Flex mt={4} align="center">
+          <Avatar size="sm" src=""/>
+          <Flex lexDir="column" ml={4} display="flex">
+              <Heading as="h3" size="sm" >Maca Álvarez</Heading>
+          </Flex>
+        </Flex>
+
+      </Flex>
 
     </Flex>
   )
