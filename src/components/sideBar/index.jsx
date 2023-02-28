1c1f315d4a278e01faa236b6af5111654713fa39
María Macarena Alvarez Castillo <mariamacarenaac@gmail.com>
2023-02-28 18:50:14 -0300

    starting with nav-item

--- src/components/sideBar/index.jsx
+++ src/components/sideBar/index.jsx
@@ -1,6 +1,9 @@
-import { Avatar, Divider, Flex, Heading } from '@chakra-ui/react'
+import { Avatar, Divider, Flex, Heading, IconButton } from '@chakra-ui/react'
+import { FiAlignJustify } from "react-icons/fi";
+import { useState } from 'react'
 
 export const SideBar = () => {
+  const [navSize, setNavSize] = useState('large')
   return (
     <Flex 
       pos="sticky"
@@ -8,29 +11,40 @@ export const SideBar = () => {
       h="95vh"
       marginTop="2.5vh"
       boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
-      borderRadius= "30px"
-      w="200px"
+      borderRadius={navSize == "small" ? "15px" : "30px"}
+      w={navSize == "small" ? "75px" : "200px"}
       flexDir="column"
       justifyContent="space-between"
     >
-      <Flex>
+      <Flex
+          p="5%"
+          flexDir="column"
+          w="100%"
+          alignItems={navSize == "small" ? "center" : "flex-start"}
+          as="nav"
+      >
         <IconButton
           background="none"
           mt={5}
           _hover={{ background: 'none' }}
-          icon={<FiMenu />}
-          onClick={() => {}}
+          icon={<FiAlignJustify />}
+          onClick={() => {
+            if (navSize == "small")
+            setNavSize("large")
+            else
+            setNavSize("small")
+          }}
         />
       </Flex>
       <Flex
         p="5%"
         flexDir="column"
         w="100%"
-        alignItems="flex-start"
+        alignItems={navSize == "small" ? "center" : "flex-start"}
         mb={4}
       >
         <Divider/>
-        <Flex mt={4} align="center">
+        <Flex mt={4} display={navSize == "small" ? "none" : "flex"}>
           <Avatar size="sm" src=""/>
           <Flex lexDir="column" ml={4} display="flex">
               <Heading as="h3" size="sm" >Maca Álvarez</Heading>
