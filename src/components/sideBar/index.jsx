1458370cf4e3efc1ed49f2ee1e79a47f859ed027
ValeriaMercado <valeria.mza93@gmail.com>
2023-03-30 00:14:02 -0300

    icons

--- src/components/sideBar/index.jsx
+++ src/components/sideBar/index.jsx
@@ -14,7 +14,7 @@ import {
   FiTrendingUp,
   FiAlignJustify,
   FiSearch,
-} from "react-icons/./Fi";
+} from "/node_modules/react-icons/Fi";
 import { useState } from "react";
 import { NavItem } from "../navItem";
 
